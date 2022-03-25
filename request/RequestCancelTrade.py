053a7af7656030fe2111c36eb0be49e4044413fe
zyr <zyr_zyr_@sohu.com>
2022-03-25 14:52:11 +0800

    clean up

--- request/RequestCancelTrade.py
+++ request/RequestCancelTrade.py
@@ -9,38 +9,38 @@ from session.Session import Session
 
 
 class RequestCancelTrade(RequestPostJson):
-    def __init__(self, session: Session, trades: List[int]) -> None:
+    def __init__(self, session: Session, trade: int) -> None:
         super().__init__(session)
-        self.trades = trades
+        self.trade = trade
 
     def get_body(self):
         di_request = [{
             '__clazz__': 'ServerRequestVO',
             'requestClass': 'TradeService',
-            'requestData': self.trades,
+            'requestData': self.trade,
             'requestId': self.session.get_post_request_id(),
             'requestMethod': 'cancelTrade'
         }]
         return self.build_body(di_request)
 
+def clear_all_trades(sess: Session):
+    # query exsisting trades
+    from request.RequestGetOwnPlayerTrades import RequestGetOwnPlayerTrades
+    request = RequestGetOwnPlayerTrades(sess)
+    response = request.post()
+    li_responses = json.loads(response.text)
+
+    li_trades = []
+    for res in li_responses:
+        if res['requestMethod'] == 'getOwnPlayerTrades':
+            li_trades = res['responseData']
+
+    # cancel all trades, must one by one
+    for trade in li_trades:
+        request = RequestCancelTrade(sess, trade['id'])
+        response = request.post()
+
 if __name__ == '__main__':
     sess = Session(sys.argv[1])
     if sess.load_from_file():
-        from request.RequestGetOwnPlayerTrades import RequestGetOwnPlayerTrades
-
-        # query exsisting trades
-        request = RequestGetOwnPlayerTrades(sess)
-        response = request.post()
-        li_responses = json.loads(response.text)
-
-        li_trades = []
-        for res in li_responses:
-            if res['requestMethod'] == 'getOwnPlayerTrades':
-                li_trades = res['responseData']
-
-        # cancel all trades
-        for trade in li_trades:
-            request = RequestCancelTrade(sess, trade['id'])
-            response = request.post()
-            li = json.loads(response.text)
-            print(json.dumps(li, indent=4))
+        clear_all_trades(sess)
